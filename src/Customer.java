4f611f9dcf3e13637a897dc765cc31be56164622
sadbhavana8623 <sadbhavanapuri123@gmail.com>
2025-04-16 14:45:27 +0530

    Add src folder

--- src/Customer.java
+++ src/Customer.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class Customer {
+    int id;
+    String firstName;
+    String lastName;
+    String city;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "city='" + getCity() + '\'' +
+                ", id=" + getId() +
+                ", firstName='" + getFirstName() + '\'' +
+                ", lastName='" + getLastName() + '\'' +
+                '}';
+    }
 }
